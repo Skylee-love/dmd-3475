8db72d8917d08a03271f7aa246908b224ec4938f
Sarah Cartagena <skc23012@uconn.edu>
2026-01-29 11:51:42 -0500

    this is my week 2 code.

---
+++ week-02/script.js
@@ -0,0 +1,26 @@
+var week = 2;
+
+console.log("Hello, this is week " + week + ".");
+
+//alert("Hello, this is really week 2");
+
+var today = "Thursday";
+//console.log(today);
+//console.log("today");
+var tomorrow = "Friday";
+
+//console.log("today is Thursday");
+//console.log("tomorrow is Friday");
+
+var greeting = "today is " + today + ".";
+
+console.log(greeting);
+
+var nextWeek = week + 1;
+console.log("next week will be week " + nextWeek);
+
+//var myName = "Sarah";
+
+var myName = prompt("what is your name?");
+
+alert("my name is " + myName);
